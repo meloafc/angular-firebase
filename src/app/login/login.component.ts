1eef2a5368f7d03d77a4bf897a2dfcf49a452160
Alex Fabiano <afabiano15@gmail.com>
2017-09-02 16:33:12 -0400

    Criando a tela de login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  model: any = {};
+  loading = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  login() {
+
+  }
+}
